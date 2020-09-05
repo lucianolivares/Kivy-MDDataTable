1e4a0749c9c466d7361471ec54798469506367d0
Luciano Olivares <lucianolivares.lo@gmail.com>
2020-09-04 20:14:28 -0400

    I added requested features in YouTube comment

--- main.py
+++ main.py
@@ -1,67 +1,103 @@
-from kivy.metrics import dp
-from kivy.lang import Builder
+import json
+import threading
 
+import requests
+from kivy.clock import mainthread
+from kivy.lang import Builder
+from kivy.metrics import dp
 from kivymd.app import MDApp
 from kivymd.uix.datatables import MDDataTable
 from kivymd.uix.floatlayout import MDFloatLayout
-from kivymd.uix.button import MDRectangleFlatButton
-import pandas as pd
 
-import threading
-from kivy.clock import Clock, mainthread
 
 Builder.load_string("""
 <RootLayout>:
+    table_box: table_box
 
     MDLabel:
-        pos_hint: {"center_x": .5, "top": .9}
+        pos_hint: {"center_x": .5, "top": 1}
         size_hint: .9, .1
         color: 0, 0, 0, 1
-        text: "MDDataTable"
-    
-    MDFlatButton:
+        text: "Viajes Disponibles"
+        halign: "center"
+        font_style: "H3"
+
+    MDBoxLayout:
+        id: table_box
+        pos_hint: {"center_x": .5, "top": .8}
+        size_hint: .9, .6
+        
+    MDFillRoundFlatButton:
         text: "Actualizar Tabla"
-        pos_hint: {"center_x": .5, "top": .2}
+        pos_hint: {"center_x": .5, "top": .1}
         on_release: root.start_second_thread()
 
 
 """
 )
 
+TRIPS_SELECTED = []
+
 class RootLayout(MDFloatLayout):
     stop = threading.Event()
 
     def start_second_thread(self):
-        threading.Thread(target=self.charge_data).start()
+        threading.Thread(target=self.load_data).start()
 
-    def charge_data(self):
-        Clock.schedule_once(self.start_test, 5)
+    def load_data(self, *args):
+        if TRIPS_SELECTED:
+            for n_trip in TRIPS_SELECTED:
+                post_request = requests.delete(f'https://remasterautostop-fc4ec.firebaseio.com/trips_available/{n_trip}.json')
 
-    def start_test(self, *args):
-        DATA = pd.read_excel("downloaded_medals.xls")
-        DATA = DATA.iloc[:, 1:]
-        cols = DATA.columns.values
-        values = DATA.values
+        get_request = requests.get(f'https://remasterautostop-fc4ec.firebaseio.com/trips_available.json')
+        trips_data = json.loads(get_request.content.decode())
+
+        count = 0
+        cols = ["Viaje"]
+        values = []
+
+        for trip, data in trips_data.items():
+            lista = []
+            lista.append(trip)
+            for key, info in data.items():
+                lista.append(info)
+                
+                if count == 0:
+                    cols.append(key)
+            count += 1
+            values.append(lista)
+
         self.update_table(cols, values)
+
+    def on_check_press(self, instance_table, current_row):
+        '''Called when the check box in the table row is checked.'''
+        if current_row[0] in TRIPS_SELECTED:
+            TRIPS_SELECTED.remove(current_row[0])
+        else:
+            TRIPS_SELECTED.append(current_row[0])
+
 
     @mainthread
     def update_table(self, cols, values):
-        self.data_table = None
+        self.table_box.clear_widgets()
 
         self.data_table = MDDataTable(
-            size_hint=(0.9, 0.6),
-            use_pagination=True,
-            column_data=[(col, dp(30)) for col in cols],
-            row_data=values
-        )
-        self.data_table.open()
+                column_data=[(col, dp(40)) for col in cols],
+                row_data=values,
+                check=True
+                )
 
+        self.data_table.bind(on_check_press=self.on_check_press)
 
+        self.table_box.add_widget(self.data_table)
 
 
 class Example(MDApp):
     def on_stop(self):
         self.root.stop.set()
+
+    def on_start(self):
+        self.root.start_second_thread()
 
     def build(self):
         return RootLayout()
